0d3585b6e3752edec51dd45fbdab40d24850018d
andre ferrer <andrerferrer@hotmail.com>
2020-10-31 12:22:29 -0300

    fix typo

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,18 @@
 import mapboxgl from 'mapbox-gl';
 
+const addMarkerToMap = (map) => {
+  const mapElement = document.getElementById('map');
+
+  // Take the marker from the view in `app/views/restaurants/show.html.erb`
+  const marker = JSON.parse(mapElement.dataset.marker);
+
+  new mapboxgl.Marker()
+    .setLngLat([ marker.lng, marker.lat ])
+    .addTo(map);
+
+}
+
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
@@ -8,11 +21,12 @@ const initMapbox = () => {
   if (mapElement) {
     // This is setting the access token as the one coming from app/views/restaurants/show.html.erb (line 11)
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
+
+    addMarkerToMap(map)
   }
 };
 
